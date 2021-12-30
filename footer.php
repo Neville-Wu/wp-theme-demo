4685f0c1cf801957f00a9407211c7e61ee389dcc
Neville <553770983@qq.com>
2021-12-30 20:46:41 +1100

    Added basic structures of header, footer and index .php

--- footer.php
+++ footer.php
@@ -1,1 +1,27 @@
 <?php
+/*
+ *
+ * */
+?>
+
+	<footer class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'demo' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Powered by %s', 'demo' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+			<?php
+			/* translators: 1: Theme name, 2: Theme author. */
+			printf( esc_html__( 'Theme: %1$s by %2$s.', 'demo' ), 'demo', '<a href="#">whom</a>' );
+			?>
+		</div>
+	</footer>
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,1 +1,52 @@
 <?php
+/*
+ *
+ * */
+?>
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="app">
+
+    <header class="site-header">
+        <div class="site-branding">
+            <?php
+            the_custom_logo();
+            if ( is_front_page() && is_home() ) :
+                ?>
+                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                <?php
+            else :
+                ?>
+                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                <?php
+            endif;
+            $demo_description = get_bloginfo( 'description', 'display' );
+            if ( $demo_description || is_customize_preview() ) :
+                ?>
+                <p class="site-description"><?php echo $demo_description; ?></p>
+            <?php endif; ?>
+        </div>
+
+        <nav class="main-navigation">
+            <?php
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'menu-1',
+                    'menu_id'        => 'primary-menu',
+                )
+            );
+            ?>
+        </nav>
+    </header>
+
